76d77fb7f73781730105c63cb4663dda531f8739
DonOutcast <lymondgl@student.21-school.ru>
2023-01-28 11:27:17 +0300

    test median end

--- Go_Day00-0/src/main.go
+++ Go_Day00-0/src/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	arrayNumbers := make([]int, 0)
-	var median int
+	var median float64
 	var lengthArray int
 	for sc.Scan() {
 		text := sc.Text()
@@ -21,10 +21,9 @@ func main() {
 	sort.Ints(arrayNumbers)
 	lengthArray = len(arrayNumbers)
 	if lengthArray%2 != 0 {
-		median = arrayNumbers[lengthArray/2]
+		median = float64(arrayNumbers[lengthArray/2])
 	} else {
-		median = (arrayNumbers[lengthArray/2] + arrayNumbers[lengthArray/2+1]) / 2
-		fmt.Println((arrayNumbers[lengthArray/2] + arrayNumbers[lengthArray/2+1]) / 2)
+		median = float64(arrayNumbers[lengthArray/2]+arrayNumbers[lengthArray/2-1]) / 2
 	}
 	fmt.Println("Median:", median)
 
