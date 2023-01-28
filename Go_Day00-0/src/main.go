dc6841385c398314979772be50fbc2d2efc1e620
DonOutcast <lymondgl@student.21-school.ru>
2023-01-28 09:09:21 +0300

    typed a median function

--- Go_Day00-0/src/main.go
+++ Go_Day00-0/src/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
-	fmt.Printf("Hello World")
+	sc := bufio.NewScanner(os.Stdin)
+	arrayNumbers := make([]int, 0)
+	var median int
+	var lengthArray int
+	for sc.Scan() {
+		text := sc.Text()
+		temp, _ := strconv.Atoi(text)
+		arrayNumbers = append(arrayNumbers, temp)
+	}
+	sort.Ints(arrayNumbers)
+	lengthArray = len(arrayNumbers)
+	if lengthArray%2 != 0 {
+		median = arrayNumbers[lengthArray/2]
+	} else {
+		median = (arrayNumbers[lengthArray/2] + arrayNumbers[lengthArray/2+1]) / 2
+		fmt.Println((arrayNumbers[lengthArray/2] + arrayNumbers[lengthArray/2+1]) / 2)
+	}
+	fmt.Println("Median:", median)
 
 }
